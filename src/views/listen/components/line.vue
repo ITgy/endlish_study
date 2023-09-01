a0b5f8f2d0f2dc12e1685d1fa59cca3a5551f7f7
gaoxianggui <gaoxianggui@7kid.com>
2023-09-01 20:49:05 +0800

    代码提交

---
+++ src/views/listen/components/line.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  line: {
+    type: Object,
+    default: () => {},
+  },
+  light: {
+    type: Boolean,
+    default: false
+  },
+  loop: {
+    type: Boolean,
+    default: false
+  },
+  show: {
+    type: Boolean,
+    default: true
+  },
+  voice: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
+<template>
+  <div class="line" :class="light ? 'light' : ''">
+    <div class="line_content" :class="show ? '' : 'mask'">
+      {{ line.text }}
+    </div>
+    <div class="line_operate">
+      <span @click="$emit('handleLoop')">单</span>
+      <span @click="$emit('handleShow')">{{
+        show ? "显" : "隐"
+      }}</span>
+      <span @click="$emit('voice')">读</span>
+    </div>
+  </div>
+</template>
+<style scope lang="scss">
+.line {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  padding: 0 0 0 20px;
+  .line_content {
+    flex: 3;
+    line-height: 20px;
+    height: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    position: relative;
+  }
+  .line_operate {
+    flex: 1;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    span {
+      display: inline-block;
+      width: 100%;
+      text-align: center;
+    }
+  }
+  .mask::after {
+    content: "";
+    display: block;
+    background-color: grey;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 10px;
+  }
+}
+.light {
+  border: 1px solid lightcoral;
+  .line_content {
+    color: burlywood;
+  }
+}
+</style>
--- src/views/listen/index.vue
+++ src/views/listen/index.vue
@@ -1,5 +1,100 @@
-<script setup></script>
+<script setup>
+import Line from './components/line.vue';
+import { ref, onMounted } from 'vue';
+
+const dataSource = [
+  {
+    text: "Lesson 1",
+    time: 0.61
+  },
+  {
+    text: "Excuse me!",
+    time: 0.61
+  },
+  {
+    text: "Listen to the tape then answer this question.",
+    time: 5.61
+  },
+  {
+    text: "Whose handbag is it?",
+    time: 10.8
+  },
+  {
+    text: "Excuse me!",
+    time: 15.11
+  },
+  {
+    text: "Yes?",
+    time: 16.66
+  },
+  {
+    text: "Is this your handbag?",
+    time: 18.26
+  },
+  {
+    text: "Pardon?",
+    time: 21.44
+  },
+  {
+    text: "Is this your handbag?",
+    time: 23.17
+  },
+  {
+    text: "Yes it is.",
+    time: 26.73
+  },
+  {
+    text: "Thank you very much.",
+    time: 29.49
+  },
+];
+
+// 获取当前滚动容器DOM对象
+const scrollWrap = ref(null);
+
+onMounted(() => {
+    console.log('scrollWrap', scrollWrap.value.clientHeight)
+    const scrollWrapHeight = scrollWrap.value.clientHeight;
+    
+})
+
+
+
+/**** start 单行控制相关事件方法 ****/
+// 是否开启单行循环
+function handleLoop(){
+
+}
+
+// 是否显示
+function handleShow(){
+
+}
+/**** end 单行控制相关事件方法 ****/
+/**** start 逻辑方法 ****/
+// 字幕滚动模式一：自动计算当前高亮行是否超出显示区域，如果超出了移动字幕保证高亮在字幕内
+function processScrollEnd(){
+
+}
+// 字幕滚动模式二：自动计算当前高亮行是否超过显示区域的中点(“中”心的“中”)，如果超出了移动字幕保证高亮在字幕内
+function processScrollMiddle(){
+    // 计算当前滚动容器的高度
+
+}
+/**** end 逻辑方法 ****/
+
+</script>
 <template>
-    练听力
+    <div class="scroll_wrap">
+        <ul class="scroll_inner" ref="scrollWrap">
+            <Line v-for="line in dataSource" :key="line.time" :line="line" @handleLoop="handleLoop" @handleShow="handleShow"></Line>
+        </ul>
+    </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.scroll_wrap {
+    height: 200px;
+    border: 1px solid #b12600;
+    overflow: auto;
+}
+</style>
